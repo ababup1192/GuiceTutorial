6c0102421756b23a5b3b79397765d08d5e6e15a9
ababup1192 <ababup1192@gmail.com>
2016-07-09 08:21:59 +0900

    Use Provider

--- src/main/scala/org/ababup1192/User.scala
+++ src/main/scala/org/ababup1192/User.scala
@@ -1,6 +1,12 @@
 package org.ababup1192
 
+import com.google.inject.Provider
+
 class User(val name: String){
   def introduce: String =
   s"My name is $name."
+}
+
+class UserProvider extends Provider[User] {
+  override def get(): User = new User("Bob")
 }
--- src/test/scala/GuiceSpec.scala
+++ src/test/scala/GuiceSpec.scala
@@ -5,9 +5,9 @@ import org.scalatest._
 class GuiceSpec extends FlatSpec with Matchers {
   "GuiceMain" should "have User Instance" in {
     val injector = Guice.createInjector(new AbstractModule {
-      override def configure(): Unit = {}
-      @Provides
-      private[this] def provideUser(): User = new User("Bob")
+      override def configure(): Unit = {
+        bind(classOf[User]).toProvider(classOf[UserProvider])
+      }
     })
 
     val main = injector.getInstance(classOf[GuiceMain])
